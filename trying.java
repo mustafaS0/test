64fdcabc68b80cf7ea0c0a0334e470b2bd264243
mustafaS0 <115041080+mustafaS0@users.noreply.github.com>
2022-10-09 15:42:11 +0300

    test2

--- trying.java
+++ trying.java
@@ -1,7 +1,24 @@
+import java.util.Scanner;
 
 public class trying {
 
+    
+    
     public static void main(String[] args) {
-        System.out.println("Hello Java");
+
+        Scanner input = new Scanner(System.in);
+        System.out.println("Enter your weight in pounds:");
+        double pounds = input.nextDouble();
+        System.out.println("Enter your height in inches:");
+        double inches = input.nextDouble();
+        double weightİnKilograms = pounds * 0.45359237 ;
+        double heightİnMeters = inches * 0.0254 ;
+        int bmi;
+        bmi = (int) (weightİnKilograms / (heightİnMeters * heightİnMeters)) ;
+        System.out.println("Your BMI is : " + bmi);
+        
+
+
+
     }
 }
